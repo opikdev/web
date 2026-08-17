55129f314b40c4c1dbd5221b05903ff3a68b00a7
opik <hi@opik.dev>
2026-08-17 17:56:23 +0700

    chore: scaffold astro project

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Required for canonical URLs, sitemap, and absolute og:image paths.
+  site: 'https://opik.dev',
+});
