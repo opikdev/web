e53a3e0a923a80f28cb8c2bd0dfd88eb5449cc91
opik <hi@opik.dev>
2026-08-17 18:05:17 +0700

    feat: add tailwind and base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   // Required for canonical URLs, sitemap, and absolute og:image paths.
   site: 'https://opik.dev',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
